c198575787ddd79bd0ea8ba1852bfa46a146ee06
Renan Germano <61218420+renangfs@users.noreply.github.com>
2024-04-12 10:25:11 -0300

    Initial commit

---
+++ src/Login.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Login extends JFrame{
+
+    public Login() {
+        setExtendedState(JFrame.MAXIMIZED_BOTH);// Define o tamanho da janela como tela cheia
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Define o comportamento padrão de fechamento
+        setVisible(true);// Torna a janela visível
+    }
+}
